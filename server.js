36744b8e83a9df5d9295a3c308a50f514cd49723
김동욱 <email@example.com>
2023-10-23 20:18:46 +0900

    3.6

--- server.js
+++ server.js
@@ -1,31 +1,41 @@
+import { PrismaClient } from "@prisma/client";
 import { ApolloServer, gql } from "apollo-server";
 
+const client = new PrismaClient(); //@prisma/cient에서 imort되는 거야 근데 이건 아까 생성된 client
+//prisma 스키마 수정 했으면 typeDefs도 수정 스키마랑 다입데프랑 통일 실켜야 한다 유일하게 수작업
 const typeDefs = gql`
   type Movie {
-    id: Int
-    title: String
-    year: Int
+    id: Int!
+    title: String!
+    year: Int!
+    genre: String
+    createdAt: String!
+    updatedAt: String!
   }
   type Query {
     movies: [Movie]
     movie: Movie
   }
   type Mutation {
-    createMovie(title: String!): Boolean
+    createMovie(title: String!, year: Int!, genre: String): Movie
     deleteMovie(title: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    movies: () => [],
+    movies: () => client.movie.findMany(), // 우리의 데이터베이스로 가서 모든 영활르 검색하게 돼
     movie: () => ({ title: "Hello", year: 2023 }),
   },
   Mutation: {
-    createMovie: (_, { title }) => {
-      console.log(title);
-      return true;
-    },
+    createMovie: (_, { title, year, genre }) =>
+      client.movie.create({
+        data: {
+          title,
+          year,
+          genre,
+        },
+      }),
     deleteMovie: (_, { title }) => {
       console.log(title);
       return true;
