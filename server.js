75648492369db8cfe64d5029715418be555d5e4e
김동욱 <email@example.com>
2023-10-23 15:56:55 +0900

    4.11

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
-const { ApolloServer, gql } = require("apollo-server");
+import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
+  type Movie {
+    id: Int
+    title: String
+    year: Int
+  }
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "world",
+    movies: () => [],
+    movie: () => ({ title: "Hello", year: 2023 }),
+  },
+  Mutation: {
+    createMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+    deleteMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
   },
 };
 
