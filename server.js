307a5c9a87aab32112be9468b80a273b84d9714e
김동욱 <email@example.com>
2023-11-03 08:19:45 +0900

    no.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { getUser } from "./users.utils";
 import client from "./client";
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.use("/static", express.static("uploads"));
 const server = new ApolloServer({
   typeDefs,
